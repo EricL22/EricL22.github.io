5ac70becebefdfa158ae1574341784a6f6bda27b
EricL22 <38116692+EricL22@users.noreply.github.com>
2022-12-25 20:20:39 -0800

    Update definitions.js

--- assets/data/definitions.js
+++ assets/data/definitions.js
@@ -1,12 +1,12 @@
 export const DEFINITIONS = {
-    14: [],
-    23: [],
-    24: [],
-    339: [],
-    482: [],
-    531: [],
-    566: [],
-    1617: [],
-    3213: [],
-    6716: []
+    14: ["table", "how many; how much; some; several"],
+    23: ["shield", "dry", "trunk"],
+    24: ["(a surname)", "in; at; on; with\n\nfrom; by\n\nthan; with regard to"],
+    339: ["to shoot", "hair"],
+    482: ["empress", "after; later"],
+    531: ["(a placename)", "to combine; to amalgamate", "to be side by side"],
+    566: ["to finish; to exhaust", "utmost; extreme; furthest"],
+    1617: ["will (future tense marker)", "general; commander"],
+    3213: ["horizontal", "perverse; unrestrained"],
+    6716: ["(a surname)", "(exclamatory particle)"]
 };
