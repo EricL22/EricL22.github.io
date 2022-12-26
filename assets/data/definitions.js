414e06590331f2509aaf6ec8c95a84e29757d444
EricL22 <38116692+EricL22@users.noreply.github.com>
2022-12-26 10:13:17 -0800

    Update definitions.js

--- assets/data/definitions.js
+++ assets/data/definitions.js
@@ -1,7 +1,7 @@
 export const DEFINITIONS = {
     14: ["table", "how many; how much; some; several"],
     23: ["shield", "dry", "trunk"],
-    24: ["(a surname)", "in; at; on; with\n\tfrom; by\n\tthan; with regard to"],
+    24: ["(a surname)", "in; at; on; with,\n\tfrom; by,\n\tthan; with regard to"],
     339: ["to shoot", "hair"],
     482: ["empress", "after; later"],
     531: ["(a placename)", "to combine; to amalgamate", "to be side by side"],
