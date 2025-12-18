10c32ee7cf6b4b471ab98ae9405c9a73a52ac7a8
EricL22 <38116692+EricL22@users.noreply.github.com>
2025-12-18 09:18:48 -0800

    Add new web games frontend

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
 import './Navigation.css'
 
 function Navigation() {
+  const { pathname } = useLocation()
+  const isHome = pathname === "/"
+
   return (
     <header className="nav">
       <nav className="nav-links">
         <a href="#">Immersive Chinese</a>
-        <a href="/unity">Web Games</a>
+        <NavLink to="/unity">Web Games</NavLink>
         <a href="/ling.html">Linguistics</a>
       </nav>
+
+      {!isHome && (
+        <NavLink to="/" className="home-button">
+          Home
+        </NavLink>
+      )}
     </header>
   )
 }
