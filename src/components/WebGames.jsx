a733c98aeb77cb6619493114ed148ccdd59251f2
EricL22 <38116692+EricL22@users.noreply.github.com>
2025-12-18 18:29:41 -0800

    Debug

--- src/components/WebGames.jsx
+++ src/components/WebGames.jsx
@@ -5,31 +5,13 @@ import './WebGames.css'
 function WebGames() {
   return (
     <main className="webgames">
-      <section className="personal-projects">
-        <div className="personal-left">
-          <h2>Personal Projects</h2>
-
-          <div className="project-block">
-            <h3>Immersive Chinese</h3>
-            <p>Learn Chinese as a Second Language just as a native would!</p>
-          </div>
-
-          <div className="project-block">
-            <h3>Web Games</h3>
-            <p>Various projects incorporating Unity’s WebGL engine.</p>
-          </div>
-
-          <div className="project-block">
-            <h3>Misc. Linguistic Ideas</h3>
-            <p>
-              Tools including character conversions and specifications. May be
-              useful for highly niche fields of East Asian studies.
-            </p>
-          </div>
-        </div>
-
+      <section className="game">
         <div className="personal-right">
           <img src={mushroom} alt="Personal project visual" />
+        </div>
+        
+        <div className="personal-left">
+          <h2>Personal Projects</h2>
         </div>
       </section>
     </main>
