2f73f332a4ff8613bc251751d58d7d4b5b0510b4
EricL22 <38116692+EricL22@users.noreply.github.com>
2025-12-18 17:09:21 -0800

    Adjust section padding

--- src/components/WebGames.jsx
+++ src/components/WebGames.jsx
@@ -6,46 +6,44 @@ function WebGames() {
   return (
     <>
       <main className="webgames">
-        <div className="page">
-          <section className="game">
-            <img src={mushroom} alt="" />
+        <section className="game">
+          <img src={mushroom} alt="" />
 
-            <div className="game-info">
-              <h2>Stuffy War</h2>
-              <p className="subtitle">Stick War-style fan game</p>
+          <div className="game-info">
+            <h2>Stuffy War</h2>
+            <p className="subtitle">Stick War-style fan game</p>
 
-              <h3>Prerelease Build</h3>
-              <ul>
-                <li>1/12/2025: Initial prototype</li>
-                <li>1/18: Added attack/death sounds and health bars</li>
-              </ul>
+            <h3>Prerelease Build</h3>
+            <ul>
+              <li>1/12/2025: Initial prototype</li>
+              <li>1/18: Added attack/death sounds and health bars</li>
+            </ul>
 
-              <a href="/unity/stuffystick"><button>Play</button></a>
-            </div>
-          </section>
+            <a href="/unity/stuffystick"><button>Play</button></a>
+          </div>
+        </section>
 
-          <section className="game">
-            <img src={mushroom} alt="" />
+        <section className="game">
+          <img src={mushroom} alt="" />
 
-            <div className="game-info">
-              <h2>Blood Typing Game</h2>
-              <p className="subtitle">
-                Learn to identify the different blood types
-              </p>
+          <div className="game-info">
+            <h2>Blood Typing Game</h2>
+            <p className="subtitle">
+              Learn to identify the different blood types
+            </p>
 
-              <h3>Prerelease Build</h3>
-              <ul>
-                <li>2/6/2021: Initial prototype</li>
-                <li>
-                  8/2022: Discontinued to the release of Nobel Prize’s <a href="https://educationalgames.nobelprize.org/educational/medicine/bloodtypinggame/index.php">2021
-                  version</a>.
-                </li>
-              </ul>
+            <h3>Prerelease Build</h3>
+            <ul>
+              <li>2/6/2021: Initial prototype</li>
+              <li>
+                8/2022: Discontinued to the release of Nobel Prize’s <a href="https://educationalgames.nobelprize.org/educational/medicine/bloodtypinggame/index.php">2021
+                version</a>.
+              </li>
+            </ul>
 
-              <a href="/unity/bloodtype"><button>Play</button></a>
-            </div>
-          </section>
-        </div>
+            <a href="/unity/bloodtype"><button>Play</button></a>
+          </div>
+        </section>
       </main>
     </>
   )
