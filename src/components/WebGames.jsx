3d44637c459b27fa5ebefecfc88a757fa52d8332
EricL22 <38116692+EricL22@users.noreply.github.com>
2025-12-18 19:01:06 -0800

    Revert "Debug"
    This reverts commit 4e3c8a75b766f88e6b01a7f802f85f86fa69da9a.

--- src/components/WebGames.jsx
+++ src/components/WebGames.jsx
@@ -5,13 +5,31 @@ import './WebGames.css'
 function WebGames() {
   return (
     <main className="webgames">
-      <section className="game">
-        <div className="personal-right">
-          <img src={mushroom} alt="Personal project visual" />
-        </div>
-        
+      <section className="personal-projects">
         <div className="personal-left">
           <h2>Personal Projects</h2>
+
+          <div className="project-block">
+            <h3>Immersive Chinese</h3>
+            <p>Learn Chinese as a Second Language just as a native would!</p>
+          </div>
+
+          <div className="project-block">
+            <h3>Web Games</h3>
+            <p>Various projects incorporating Unity’s WebGL engine.</p>
+          </div>
+
+          <div className="project-block">
+            <h3>Misc. Linguistic Ideas</h3>
+            <p>
+              Tools including character conversions and specifications. May be
+              useful for highly niche fields of East Asian studies.
+            </p>
+          </div>
+        </div>
+
+        <div className="personal-right">
+          <img src={mushroom} alt="Personal project visual" />
         </div>
       </section>
     </main>
